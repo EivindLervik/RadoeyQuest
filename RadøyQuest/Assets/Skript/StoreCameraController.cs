e9846dad1b12fcb24efc64efbed0bec316e7fba2
EivindLervik <eivind_fantaboy@msn.com>
2017-01-29 00:47:48 +0100

    Added the handelslaget inside scene

---
+++ RadøyQuest/Assets/Skript/StoreCameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreCameraController : MonoBehaviour {
+
+    public float transitionSpeedModefier;
+    public int currentIndex;
+    public Transform[] states;
+
+    private Transform targetTransform;
+
+    void Start()
+    {
+        SetTargetTransform();
+    }
+
+	void Update () {
+        // Update position
+        transform.position = Vector3.Lerp(transform.position, targetTransform.position, Time.deltaTime * transitionSpeedModefier);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetTransform.rotation, Time.deltaTime * transitionSpeedModefier);
+
+        // Change position
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            if (Input.GetAxis("Horizontal") > 0.0f){
+                currentIndex++;
+            }
+            else
+            {
+                currentIndex--;
+            }
+            
+
+            if (currentIndex >= states.Length)
+            {
+                currentIndex = 0;
+            }
+            else if( currentIndex < 0)
+            {
+                currentIndex = states.Length - 1;
+            }
+
+            SetTargetTransform();
+        }
+	}
+
+    private void SetTargetTransform()
+    {
+        targetTransform = states[currentIndex];
+    }
+}
