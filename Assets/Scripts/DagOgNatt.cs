3494f1027e4418208cbf7028afd3665fe2a79d0d
EivindLervik <eivind_fantaboy@msn.com>
2017-01-31 19:18:37 +0100

    Started on Hauglandbygget

--- Assets/Scripts/DagOgNatt.cs
+++ Assets/Scripts/DagOgNatt.cs
@@ -3,14 +3,38 @@ using System.Collections;
 
 public class DagOgNatt : MonoBehaviour {
 
-	public float dagFart;
-	
-	// Use this for initialization
+    public int minutes;
+    public int hours;
+	public float speedModefier;
+
+    private Light light;
+    private float realTime;
+
 	void Start () {
+        light = GetComponentInChildren<Light>();
 	}
-	
-	// Update is called once per frame
+
 	void FixedUpdate () {
-		transform.RotateAround(transform.position, transform.up, 1.0f * Time.deltaTime * dagFart);
+        // Update sun rotation
+        Vector3 rot = transform.eulerAngles;
+        rot.x = ((minutes + (hours * 60)) / 1440.0f) * 360.0f;
+        transform.Rotate(Vector3.right, (Time.fixedDeltaTime * speedModefier) / Mathf.PI);
+
+        // Calculate time
+        realTime += Time.fixedDeltaTime * speedModefier;
+        minutes = (int) ((transform.rotation.x / 360.0f) * 60.0f);
+        if(minutes >= 60)
+        {
+            hours++;
+
+            if(hours >= 24)
+            {
+                hours = 0;
+            }
+        }
+        if (realTime >= 60.0f)
+        {
+            realTime -= 60.0f;
+        }
 	}
 }
